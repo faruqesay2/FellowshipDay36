70fac7b026e5abbb6e86b75690c8ed05c1845607
faruqesay2 <faruqesay786@gmail.com>
2022-12-19 23:02:18 +0530

    Multthreading

---
+++ EmployeePayRollUsingThread/EmployeePayRollUsingThread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRollUsingThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Multi Threading Problem");
+        }
+    }
+}
